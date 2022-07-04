1679d678e4cd12ca3183920197826c4b82742011
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-04 16:23:34 +0200

    update fizz buzz file

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -5,7 +5,7 @@ def fizzbuzz():
             print("Fizz ", end="")
         elif num % 5 == 0:
             print("Buzz ", end="")
-        elif num % 15 == 0:
+        elif num % 5 == 0 and num % 3 == 0:
             print("FizzBuzz ", end="")
         else:
             print(num, end=" ")
