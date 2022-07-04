90d7f6a1687a7b66ae1cec899778539fd3d4d34c
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-04 16:15:10 +0200

    update fizz buzz file

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for num in range(101):
         if num % 3 == 0:
             print("Fizz ", end=" ")
-        elif num % 5 == o:
+        elif num % 5 == 0:
             print("Buzz ", end=" ")
         elif num % 15 == 0:
             print("FizzBuzz ", end=" ")
