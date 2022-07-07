119fbfa5deba2bdb5aae0fa1bb5a04ff22d1d5cc
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-07 16:22:45 +0200

    update args file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    args = len(sys.argv)
-    print("{}".format(args - 1), end="")
-    if args == 1:
-        print("argument:")
-    elif args > 1:
-        if args == 2:
-            print("arguments:")
-        elif args > 2:
-            print("arguments:")
-        for num in range(args - 1):
-            print("{}: {}".format(( num + 1), sys.argv[num + 1]))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
