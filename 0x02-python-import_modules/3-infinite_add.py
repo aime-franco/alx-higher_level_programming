f7314e4aa3414a51c6d59a4c19d768137577e357
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-07 16:36:45 +0200

    create infinite add file

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    solution = 0
+    for i in range(1,len(sys.argv)):
+        solution += int(sys.argv[i])
+    print("{}".format(result))
