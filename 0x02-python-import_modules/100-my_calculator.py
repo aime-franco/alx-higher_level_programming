341da127f61e8cc811866e57466315d437a258fc
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-07 18:38:38 +0200

    update my calculator file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
-     from calculator_1 import add, sub, mul, div
-    import sys
-
-    if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
-
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+if __name__ == "__main__":
+    from sys import argv
+    temp = 0
+    numero2 = 0
+    for i in range(1, len(argv)):
+        numero = int(argv[i])
+        resultado = numero + numero2
+        numero2 = resultado
+        temp = resultado
+    print("{}".format(temp))
