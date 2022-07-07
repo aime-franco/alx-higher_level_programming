d431f7701ed5ea2a8d1c76021d3c60e174380eda
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-07 16:39:06 +0200

    update infinite add file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     solution = 0
     for i in range(1,len(sys.argv)):
         solution += int(sys.argv[i])
-    print("{}".format(result))
+    print("{}".format(solution))
