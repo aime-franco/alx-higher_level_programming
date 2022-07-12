a72a27f91a311fa5af187b408399f1b82bc44b10
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-12 09:41:58 -0700

    update concatinating words file

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\language that combines remarkable power with very clear synthax"
-str = str[39:66] + ' ' + str[107:112] + str[:6]
-print(str)
+print(str[39:66] + ' ' + str[107:112] + str[:6])
