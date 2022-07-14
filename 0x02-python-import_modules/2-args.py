cdb1ecd414c4bb4b76e297c2b924cddf8c95d1c3
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-15 01:11:49 +0200

    update the system argument module file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,9 @@ if __name__ == "__main__":
     from sys import argv
     print("{} argument".format(len(argv) - 1), end="")
     if len(argv) == 0:
-        print("arguments.")
+        print("argument.")
     elif args > 1:
-        if args == 2:
+        if args == 1:
             print("argument:")
         elif args > 2:
             print("arguments:")
