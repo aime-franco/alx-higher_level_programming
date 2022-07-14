6a8da917215c311fc5a93b66f0bde0aa6b616d40
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-15 01:39:41 +0200

    update the summing up the numbers continously file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    solution = 0
-    temp = 0
-    for i in range(1, len(sys.argv)):
-        number = int(sys.argv[i])
-        number2 = number + temp
-        solution = number
-    print("{}".format(solution))
+    numbers = 0
+    for num in range(1, len(argv)):
+        numbers += argv[num]
+    print("{}".format(numbers))
