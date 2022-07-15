88e0ef9a9be35a2ef4a4ba5f79eff2b3e59b2483
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-16 01:02:16 +0200

    delete 3-infinite_add.py file

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    addition = 0
-    for i in range(len(argv) - 1):
-        addition += int(argv[i + 1])
-    print("{}".format(addition))
