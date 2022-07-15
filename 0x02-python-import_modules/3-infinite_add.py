3b392ecc3427867b033f9f5f16bbadbe0e04b627
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-16 01:15:57 +0200

    create file which adds all the argument

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    args_number = len(argv)
+    import sys
     addition = 0
-    for i in range(1, args_number):
-        addition += int(argv[i])
+    for i in range(len(argv) - 1):
+        addition += int(argv[i + 1])
     print("{}".format(addition))
