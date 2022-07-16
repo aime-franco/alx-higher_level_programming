1302b025bd6a866fe5d68adb31064c73ac5518ef
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-17 00:32:33 +0200

    discovery hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     names = dir(hideden_4)
     for name in names:
-        if name != "__":
-            print("{:d}".format(name))
+        if name[:2] != "__":
+            print(name)
