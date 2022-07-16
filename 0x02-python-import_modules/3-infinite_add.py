bdaa75eeabca430b0d4ef5a4fdb6b1fda60a52da
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-17 00:12:31 +0200

    add infinite file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        a = 0
-        for i in sys.argv:
-            if i != sys.argv:
-                c += int(i)
-        print("{:d}".format(c))
-    else:
-        print(0)
+    import sys
+    args_number = len(sys.argv)
+    summation = 0
+    for i in range(1, args_number):
+        summation += int(sys.argv[i])
+    print("{:d}".format(summation))
