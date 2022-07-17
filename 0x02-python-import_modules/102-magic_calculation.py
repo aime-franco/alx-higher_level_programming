f7ec5f85ccbb116842cb44389038bfdd85dc8c5c
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-17 19:55:33 +0200

    update bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
         c = add(a, b)
         for i in range(4, 6):
             c  = add(c, i)
-        return (c)
+            return (c)
     else:
         return (sub(a, b))
