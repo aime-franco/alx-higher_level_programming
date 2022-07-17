f1873669fc0d2a7b981220f458690f068108fdfb
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-17 13:32:56 +0200

    update import calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,21 +2,20 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    args = sys.argv
-    if len(args) != 4:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     ops =("+", "-", "/", "*")
-    if args[2] not in ops:
+    if sys.argv[2] not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    a = int(args[2])
-    b = int(args[2])
-    if args[2] == "+":
-        print("{:d}  {:d} = { :d}".format(a, args[2], b, add(a, b)))
-    elif args[2] == "-":
-        print("{:d}  {:d} = { :d}".format(a, args[2], b, sub(a, b)))
-    elif args[2] == "*":
-        print("{:d}  {:d} = { :d}".format(a, args[2], b, mul(a, b)))
-    elif args[2] == "/":
-        print("{:d}  {:d} = { :d}".format(a, args[2], b, div(a, b)))
+    a = int(sys.argv[2])
+    b = int(sys.argv[2])
+    if sys.argv[2] == "+":
+        print("{:d}  {:d} = { :d}".format(a, sys.argv[2], b, add(a, b)))
+    elif sys.argv[2] == "-":
+        print("{:d}  {:d} = { :d}".format(a, sys.argv[2], b, sub(a, b)))
+    elif sys.argv[2] == "*":
+        print("{:d}  {:d} = { :d}".format(a, sys.argv[2], b, mul(a, b)))
+    elif sys.argv[2] == "/":
+        print("{:d}  {:d} = { :d}".format(a, sys.argv[2], b, div(a, b)))
