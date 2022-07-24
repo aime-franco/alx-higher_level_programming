0207a28bc33d3fba0aedbaad7757aaa8f26e6d06
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-24 05:58:04 +0200

    update no c file

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,7 +3,8 @@ def no_c(my_string):
      new_string = list(my_string)
      index = 0
      for i in new_string:
-         new_string[index] = '' 
+         new_string[index] = ''
          if (i == 'c' or i == 'C'):
              index += 1
-     return ''.join(new_string)
+         else:
+             return (''.join(new_string))
