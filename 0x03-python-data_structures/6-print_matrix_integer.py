6a4d2a055829226e711ed5e820ee4c5d6eda35e3
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-27 01:44:04 +0200

    create print matrix file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for num in matrix:
         for col in num:
-            print("{:d}".format(col), end=" " if col != row[-1]  else "")
-        print()
+            if num != col:
+                print("{:d}".format(col), end=" ")
+            else:
+                print()
