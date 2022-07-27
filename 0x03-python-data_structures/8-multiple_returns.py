eaccaf2b5b1783cfb72f119917eefb54196ba9c9
aime-franco <usanaseaimefrancois@gmail.com>
2022-07-27 06:45:24 +0200

    create multiple returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    Mytuple = ()
+    if len(sentence) == 0:
+        Mytuple = 0, "None"
+    else:
+        Mytuple = len(sentence), sentence[0]
+    return Mytuple
+     
