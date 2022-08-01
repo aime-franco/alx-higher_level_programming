1d2171e65711363841b6ab0a593e5cffb47710ee
aime-franco <usanaseaimefrancois@gmail.com>
2022-08-01 14:46:46 +0200

    create search replace file

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+    for i in my_list:
+        if i == search:
+            my_list.append(replace)
+        else:
+            my_list.append(i)
+    return my_list
