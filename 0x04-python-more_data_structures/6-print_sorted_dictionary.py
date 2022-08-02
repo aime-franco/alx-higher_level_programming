e6206fa94e507a7a7b227b4e54d5b6ab7cef33ac
aime-franco <usanaseaimefrancois@gmail.com>
2022-08-02 10:40:31 +0200

    update print sorted dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dicitionary(a_dictionary):
-    new_string = ''
+    new_string = {}
     for item in a_dictionary:
         new_string.sort(a_dictionary)
     return new_string
