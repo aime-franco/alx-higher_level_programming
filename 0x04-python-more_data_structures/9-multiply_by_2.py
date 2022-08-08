21382ebf15fc829edfbfd0d41654cd9fc2200584
aime-franco <usanaseaimefrancois@gmail.com>
2022-08-08 13:00:28 +0200

    create multiply by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    product = 0
+    for i in a_dictionary:
+        product = i * 2
+        a_dictionary.append[product]
+    return a_dictionary
