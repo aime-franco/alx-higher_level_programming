5ec1a57e6ed590da3d27b80c045d9254ac0829cf
aime-franco <usanaseaimefrancois@gmail.com>
2022-08-09 03:00:41 +0200

    update best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    new_value = {}
+    new_value = max(a_dictionary, key=a_dictionary.get)
     for i in a_dictionary:
         if i not in a_dictionary:
             return None
-        else:
-            new_value = {}
-            new_value = max(a_dictionary, key=a_dictionary.get)
-    return new_value
