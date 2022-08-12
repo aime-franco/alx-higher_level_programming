b9789e1c41883e0f47daba5085d9ff559c99c554
aime-franco <usanaseaimefrancois@gmail.com>
2022-08-12 12:20:43 +0200

    create safe print division

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        quotient = int(a) / int (b)
+        return quotient
+    except (ValueError, ZeroDivisionError):
+        return None
+    finally:
+        print("{}".format(quotient))
