15cff9770dc57c138d654b3bfbf1f364e74f44fc
aime-franco <usanaseaimefrancois@gmail.com>
2022-08-12 09:04:35 +0200

    update safe print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    output = 0
-    for i in value:
-        try:
-            print("{}".format(value))
-            output += 1
-            if value != str(value):
-                return True
-            else:
-                return False
-        except TypeError:
-            break
-    print()
-    return output
+def safe_print_intefer(value):
+    try:
+        print("{}".format(value))
+        return True
+    except (TypeError, ValueError):
+        return False
